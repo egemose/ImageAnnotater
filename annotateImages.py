1a1402c18765d5214f1b2af015a94206f9b9df2a
Henrik Egemose <hes1990@gmail.com>
2017-07-14 16:57:14 +0200

    added opportunity to open file

--- annotateImages.py
+++ annotateImages.py
@@ -1,37 +1,51 @@
 import cairo
 import gi
+from collections import namedtuple
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk, GdkPixbuf
 
 
 class Handler:
     def __init__(self, gui_builder):
+        self.buf_and_image = namedtuple('buf_and_image', ['buf', 'image'])
+        self.main_window = gui_builder.get_object('main_window')
         self.overlay = gui_builder.get_object('overlay')
-        self.background_image = gui_builder.get_object('background_image')
-        self.original_image = gui_builder.get_object('original_image')
-        self.bw_image = gui_builder.get_object('bw_image')
-        self.draw_image = gui_builder.get_object('drawing_image')
         self.label_zoom_level = gui_builder.get_object('zoom_level')
-        self.background_buf = self.background_image.get_pixbuf()
-        self.original_buf = self.original_image.get_pixbuf()
-        self.bw_buf = self.bw_image.get_pixbuf()
-        self.draw_buf = self.draw_image.get_pixbuf()
-
+        background_image = gui_builder.get_object('background_image')
+        original_image = gui_builder.get_object('original_image')
+        bw_image = gui_builder.get_object('bw_image')
+        draw_image = gui_builder.get_object('drawing_image')
+        background_buf = background_image.get_pixbuf()
+        original_buf = original_image.get_pixbuf()
+        bw_buf = bw_image.get_pixbuf()
+        draw_buf = draw_image.get_pixbuf()
+        background = self.buf_and_image(background_buf, background_image)
+        original = self.buf_and_image(original_buf, original_image)
+        bw = self.buf_and_image(bw_buf, bw_image)
+        draw = self.buf_and_image(draw_buf, draw_image)
+        self.buffers_and_images = {'background': background,
+                                   'original': original,
+                                   'bw': bw,
+                                   'draw': draw}
         self.scale = 1
+        self.image_width = 320
+        self.image_height = 320
 
     @staticmethod
     def delete_window(*args):
         Gtk.main_quit(*args)
 
     def switch_images(self, button):
+        original = self.buffers_and_images.get('original')
+        bw = self.buffers_and_images.get('bw')
         if button.get_active():
-            self.overlay.reorder_overlay(self.original_image, 0)
-            self.overlay.reorder_overlay(self.bw_image, 1)
+            self.overlay.reorder_overlay(original.image, 0)
+            self.overlay.reorder_overlay(bw.image, 1)
         else:
-            self.overlay.reorder_overlay(self.original_image, 1)
-            self.overlay.reorder_overlay(self.bw_image, 0)
+            self.overlay.reorder_overlay(original.image, 1)
+            self.overlay.reorder_overlay(bw.image, 0)
 
-    def zoom(self, button):
+    def zoom_pressed(self, button):
         value = 0.5
         if button.get_label() == '-':
             value = -value
@@ -39,48 +53,87 @@ class Handler:
             pass
         else:
             self.scale = self.scale + value
-            width = self.scale * 320
-            background_buf = self.background_buf.scale_simple(width,
-                                                              width,
-                                                              GdkPixbuf.InterpType.BILINEAR)
-            original_buf = self.original_buf.scale_simple(width,
-                                                          width,
-                                                          GdkPixbuf.InterpType.BILINEAR)
-            bw_buf = self.bw_buf.scale_simple(width,
-                                              width,
-                                              GdkPixbuf.InterpType.BILINEAR)
-            draw_buf = self.draw_buf.scale_simple(width,
-                                                  width,
-                                                  GdkPixbuf.InterpType.BILINEAR)
-            self.background_image.set_from_pixbuf(background_buf)
-            self.original_image.set_from_pixbuf(original_buf)
-            self.bw_image.set_from_pixbuf(bw_buf)
-            self.draw_image.set_from_pixbuf(draw_buf)
-            self.label_zoom_level.set_markup(str(self.scale))
+            self.zoom()
+
+    def zoom(self):
+        width = self.image_width * self.scale
+        height = self.image_height * self.scale
+        for buf_and_image in self.buffers_and_images.values():
+            buf_temp = buf_and_image.buf.scale_simple(width,
+                                                      height,
+                                                      GdkPixbuf.InterpType.BILINEAR)
+            buf_and_image.image.set_from_pixbuf(buf_temp)
+        self.label_zoom_level.set_markup(str(self.scale))
 
     def draw_circle(self, event_box, event):
         print('event at: (%f, %f)' % (event.x, event.y))
-        draw_buf = self.draw_image.get_pixbuf()
+        draw = self.buffers_and_images.get('draw')
+        draw_buf = draw.image.get_pixbuf()
         width = draw_buf.get_width()
-        print(width)
         height = draw_buf.get_height()
-        print(height)
         surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, width, height)
         cr = cairo.Context(surface)
         Gdk.cairo_set_source_pixbuf(cr, draw_buf, 0, 0)
         cr.paint()
-        cr.set_source_rgba(0, 0, 0, 0.5)
+        cr.set_source_rgba(1, 0, 0, 1)
         cr.rectangle(int(event.x)-5, int(event.y)-5, 10, 10)
         cr.fill()
         surface = cr.get_target()
-        self.draw_buf = Gdk.pixbuf_get_from_surface(surface, 0, 0, width,
-                                                    height)
-        self.draw_image.set_from_pixbuf(self.draw_buf)
+        draw_buf = Gdk.pixbuf_get_from_surface(surface, 0, 0, width, height)
+        new_draw = self.buf_and_image(draw_buf, draw.image)
+        self.buffers_and_images['draw'] = new_draw
+        draw.image.set_from_pixbuf(draw_buf)
+
+    @staticmethod
+    def add_filters(dialog):
+        filter_jpg = Gtk.FileFilter()
+        filter_jpg.set_name("JPG images")
+        filter_jpg.add_mime_type("image/jpeg")
+        dialog.add_filter(filter_jpg)
+
+        filter_png = Gtk.FileFilter()
+        filter_png.set_name("Png images")
+        filter_png.add_mime_type("image/png")
+        dialog.add_filter(filter_png)
+
+        filter_any = Gtk.FileFilter()
+        filter_any.set_name("Any files")
+        filter_any.add_pattern("*")
+        dialog.add_filter(filter_any)
+
+    def open_image(self, filename):
+        original = self.buffers_and_images.get('original')
+        original.image.set_from_file(filename)
+        new_buf = original.image.get_pixbuf()
+        new_original = self.buf_and_image(new_buf,
+                                          original.image)
+        self.buffers_and_images['original'] = new_original
+        self.scale = 1
+        self.image_width = new_original.buf.get_width()
+        self.image_height = new_original.buf.get_height()
+        self.zoom()
+
+    def open_image_dialog(self, open_button):
+        dialog = Gtk.FileChooserDialog("Please choose a image",
+                                       self.main_window,
+                                       Gtk.FileChooserAction.OPEN,
+                                       (Gtk.STOCK_CANCEL,
+                                        Gtk.ResponseType.CANCEL,
+                                        Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+        self.add_filters(dialog)
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            print("Open clicked")
+            print("File selected: " + dialog.get_filename())
+            self.open_image(dialog.get_filename())
+        elif response == Gtk.ResponseType.CANCEL:
+            print("Cancel clicked")
+        dialog.destroy()
 
 
 if __name__ == '__main__':
     builder = Gtk.Builder()
-    builder.add_from_file("test4.glade")
+    builder.add_from_file("GUI.glade")
     signal_handler = Handler(builder)
     builder.connect_signals(signal_handler)
     window = builder.get_object("main_window")
